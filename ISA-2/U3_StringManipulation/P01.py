185b3bddc1ba6b4736dea1cf49d8d37be67a5286
Adnan-Safdari <safdariadnan@gmail.com>
2024-09-23 12:08:08 +0530

    FINAL

--- ISA-2/U3_StringManipulation/P01.py
+++ ISA-2/U3_StringManipulation/P01.py
@@ -14,8 +14,6 @@ print(my_string4)  # Output: Hello
 my_str = 'programming'
 print('my_str=', my_str)  # Output: mystr= programming
 
-
-
 # - - - - - - - - - - - - - SLICING STRINGS - - - - - - - - - - - - -
 
 print('my_str[0]=', my_str[0])  # Output: my_str[0]= p
@@ -39,14 +37,44 @@ try:
 except NameError:  # Handling the error as the string is deleted
     print("The var 'temp_string' does not exist")
 
-
 # - - - - - - - - - - - - - HANDLING STRINGS WITH DIGITS - - - - - - - - - - - - -
 
-string_filter_digits = "Hello World 123"
+string_filter_digits = "Hello 321 World 123"
 
 # Checking if all characters are a digit
-result_filter_digits = all(char.isdigit() for char in string_filter_digits)
-print(result_filter_digits)  # Output: False
+result_digits_check = all(char.isdigit() for char in string_filter_digits)
+print(result_digits_check)  # Output: False
+
+# Checking if any are digits
+result_digits_check = any(char.isdigit() for char in string_filter_digits)
+print(result_digits_check)  # Output: True
+
+# - - - - - - - - - - - - - IN BUILT FUNCTIONS FOR STRINGS - - - - - - - - - - - - -
+
+myVarForMaxMin = "HelloWorld89"
+
+# Maximum
+print(f"Max: {max(myVarForMaxMin)}")  # Output: Max:r
+
+# Minimum
+print(f"Min: {min(myVarForMaxMin)}")  # Output: Min:r
+
+# Type
+print(f"Type: {type(myVarForMaxMin)}")  # Output:  <class 'str'>
+
+# Length
+print(f"Length: {len(myVarForMaxMin)}")  # Output: Length: 12
+
+# ID
+print(f"ID: {id(myVarForMaxMin)}")  # Output: ID:  2733265750064
 
 
+# as_integer_ratio()
+myFloatingNumber = float("5.5")  # Converting string to float
+print(f"Ratio: {myFloatingNumber.as_integer_ratio()}")  # Output: (11.2)
+# Checking the type
+print(f"Ratio: {type(myFloatingNumber.as_integer_ratio())}")  # Output: Tuple
 
+# bit_length()
+bit_check_number = 15
+print(f"Bit Length: {bit_check_number.bit_length()}")  # Output: 4
